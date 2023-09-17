b38d8bee1c33eb4e7ae244d38c6312c65b01a369
Marius Bledea <marius.bledea@accesa.eu>
2023-09-17 22:17:57 +0300

    Change website

---
+++ src/screens/Section/Section.jsx
@@ -0,0 +1,341 @@
+import React from "react";
+import "./style.css";
+
+export const Section = () => {
+  return (
+    <div className="section">
+      <div className="div">
+        <div className="overlap">
+          <img className="vector" alt="Vector" src="/img/vector.svg" />
+          <div className="placeholder-image">
+            <div className="group">
+              <div className="overlap-group">
+                <img className="adobestock" alt="Adobestock" src="/img/adobestock-124201476-1.png" />
+                <div className="rectangle" />
+                <div className="rectangle-2" />
+                <div className="rectangle-3" />
+              </div>
+            </div>
+          </div>
+          <div className="frame">
+            <div className="frame-2">
+              <div className="frame-3">
+                <div className="text-wrapper">Marius Bledea</div>
+                <div className="devops-engineer">DevOps Engineer&nbsp;&nbsp;|&nbsp;&nbsp;Cloud Architect</div>
+              </div>
+              <p className="p">
+                DevOps Engineer experienced in
+                <br />
+                Azure, Terraform, Kubernetes, and
+                <br />
+                Cloud Native Solutions.
+              </p>
+            </div>
+            <div className="frame-4">
+              <button className="button">
+                <div className="link-download-resume">Get in touch</div>
+              </button>
+              <button className="link-download-resume-wrapper">
+                <div className="link-download-resume-2">Download Resume</div>
+              </button>
+            </div>
+          </div>
+          <div className="frame-5">
+            <div className="text-wrapper-2">About Me</div>
+            <img className="line" alt="Line" src="/img/line-1-6.svg" />
+          </div>
+          <p className="i-am-a-devops">
+            I am a DevOps Engineer experienced in Azure, Terraform, Kubernetes, and Cloud Native Solutions. I&#39;m
+            specialized in designing and implementing cloud infrastructure, automating deployment processes, and
+            optimising application performance. <br />
+            <br />
+            With my expertise I can help streamline your development and deployment workflows. Contact me to discuss
+            your project requirements and find out how I can contribute to your team.
+          </p>
+          <div className="frame-6">
+            <div className="text-wrapper-3">Completed Projects</div>
+            <p className="text-wrapper-4">
+              Managed and optimized Azure infrastructure using Terraform. Implemented and maintained Kubernetes
+              clusters. Developed and deployed cloud native solutions to enhance application performance and
+              scalability.
+            </p>
+          </div>
+          <div className="frame-7">
+            <div className="text-wrapper-3">Clients Satisfied</div>
+            <p className="text-wrapper-4">
+              DevOps Engineer with expertise in Azure, Terraform, Kubernetes, and Cloud Native Solutions.
+            </p>
+          </div>
+          <div className="frame-8">
+            <div className="text-wrapper-5">75</div>
+            <div className="text-wrapper-6">+</div>
+          </div>
+          <div className="frame-9">
+            <div className="text-wrapper-5">25</div>
+            <div className="text-wrapper-6">+</div>
+          </div>
+          <div className="frame-10">
+            <div className="profile-picture">
+              <div className="mask-group-wrapper">
+                <img className="mask-group" alt="Mask group" src="/img/mask-group.png" />
+              </div>
+            </div>
+            <div className="frame-11">
+              <img className="image" alt="Image" src="/img/image.svg" />
+              <p className="text-wrapper-7">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+          <div className="text-wrapper-8">DevOps Engineer</div>
+          <div className="frame-12">
+            <div className="frame-13">
+              <div className="text-wrapper-2">Skills and Expertise</div>
+              <img className="line" alt="Line" src="/img/line-1-5.svg" />
+            </div>
+            <div className="frame-14">
+              <div className="frame-15">
+                <img className="img" alt="Vector" src="/img/vector-8.svg" />
+                <p className="text-wrapper-9">Azure Infrastructure Design and Implementation</p>
+                <p className="text-wrapper-4">
+                  Implements and manages Azure infrastructure using Terraform. Proficient in container orchestration
+                  with Kubernetes.
+                </p>
+              </div>
+              <div className="frame-16">
+                <img className="vector-2" alt="Vector" src="/img/vector-7.svg" />
+                <p className="text-wrapper-9">Cloud Infrastructure Design and Optimization</p>
+                <p className="text-wrapper-4">
+                  Designs and deploys cloud native solutions to optimize application performance and reliability.
+                </p>
+              </div>
+              <div className="frame-16">
+                <img className="vector-3" alt="Vector" src="/img/vector-6.svg" />
+                <div className="text-wrapper-9">Kubernetes and Container Orchestration</div>
+                <p className="text-wrapper-10">
+                  mplements and manages Azure infrastructure using Terraform. Proficient in container orchestration with
+                  Kubernetes.
+                </p>
+              </div>
+              <div className="frame-15">
+                <img className="vector-4" alt="Vector" src="/img/vector-5.svg" />
+                <div className="text-wrapper-9">Terraform,&nbsp;&nbsp;Cloud Native Solutions</div>
+                <p className="text-wrapper-4">
+                  Implements and manages Azure infrastructure using Terraform. Proficient in container orchestration
+                  with Kubernetes.
+                </p>
+              </div>
+              <div className="frame-16">
+                <img className="vector-5" alt="Vector" src="/img/vector-4.svg" />
+                <div className="text-wrapper-9">Kubernetes and Container Orchestration</div>
+                <p className="text-wrapper-11">
+                  Implements and manages Azure infrastructure using Terraform. Proficient in container orchestration
+                  with Kubernetes.
+                </p>
+              </div>
+              <div className="frame-16">
+                <img className="vector-6" alt="Vector" src="/img/vector-3.svg" />
+                <div className="text-wrapper-9">Kubernetes and Container Orchestration</div>
+                <p className="text-wrapper-12">
+                  Implements and manages Azure infrastructure using Terraform. Proficient in container orchestration
+                  with Kubernetes.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-17">
+          <div className="frame-13">
+            <div className="text-wrapper-2">Work Experience</div>
+            <img className="line" alt="Line" src="/img/line-1.svg" />
+          </div>
+          <div className="frame-18">
+            <div className="frame-19">
+              <div className="frame-20">
+                <div className="frame-21">
+                  <div className="text-wrapper-13">Company Name</div>
+                  <div className="div-wrapper">
+                    <div className="text-wrapper-14">Senior DevOps Engineer</div>
+                  </div>
+                </div>
+                <div className="text-wrapper-14">July 2021 - Present</div>
+              </div>
+              <p className="text-wrapper-15">
+                Developed and deployed Azure infrastructure using Terraform. Collaborated with teams to manage and scale
+                Kubernetes clusters. Implemented cloud native solutions for applications.
+                <br />
+                <br />
+                Automated deployment and management of Azure resources using Terraform. Coordinated and scaled
+                Kubernetes clusters. Developed and implemented cloud native solutions for application optimization.
+              </p>
+            </div>
+            <img className="line" alt="Line" src="/img/line-5.svg" />
+            <div className="frame-22">
+              <div className="frame-23">
+                <div className="frame-21">
+                  <div className="text-wrapper-13">Company Name</div>
+                  <div className="div-wrapper">
+                    <div className="text-wrapper-14">DevOps Engineer</div>
+                  </div>
+                </div>
+                <p className="text-wrapper-14">Oct 2020 - July 2021</p>
+              </div>
+              <p className="text-wrapper-15">
+                Developed and implemented Terraform infrastructure-as-code to provision and manage Azure resources.
+                Orchestrated containerized applications using Kubernetes. Utilized cloud native solutions to optimize
+                application scalability and reliability.
+              </p>
+            </div>
+            <img className="line" alt="Line" src="/img/line-5.svg" />
+            <div className="frame-24">
+              <div className="frame-25">
+                <div className="frame-21">
+                  <div className="text-wrapper-13">Company Name</div>
+                  <div className="div-wrapper">
+                    <div className="text-wrapper-14">Application Engineer</div>
+                  </div>
+                </div>
+                <p className="text-wrapper-14">May 2018 - Oct 2020</p>
+              </div>
+              <p className="text-wrapper-15">
+                Developed and deployed Azure infrastructure using Terraform. Collaborated with teams to manage and scale
+                Kubernetes clusters. Implemented cloud native solutions for applications..
+              </p>
+            </div>
+            <img className="line" alt="Line" src="/img/line-5.svg" />
+          </div>
+        </div>
+        <div className="frame-26">
+          <div className="frame-13">
+            <div className="text-wrapper-2">Certificates</div>
+            <img className="line" alt="Line" src="/img/line-1.svg" />
+          </div>
+          <div className="frame-27">
+            <div className="frame-28">
+              <div className="image-wrapper">
+                <img className="image-2" alt="Image" src="/img/image-8.png" />
+              </div>
+              <div className="frame-29">
+                <p className="text-wrapper-16">Microsoft Certified: Azure Solutions Architect Expert</p>
+              </div>
+            </div>
+            <div className="frame-28">
+              <div className="image-wrapper">
+                <img className="image-2" alt="Image" src="/img/image-9.png" />
+              </div>
+              <div className="frame-29">
+                <p className="text-wrapper-16">HashiCorp Certified: Terraform Associate (002)</p>
+              </div>
+            </div>
+            <div className="frame-28">
+              <div className="image-wrapper">
+                <img className="image-2" alt="Image" src="/img/image-10.png" />
+              </div>
+              <div className="frame-29">
+                <p className="text-wrapper-16">Microsoft Certified: Azure Security Engineer Associate</p>
+              </div>
+            </div>
+            <div className="frame-28">
+              <div className="image-wrapper">
+                <img className="image-2" alt="Image" src="/img/image-11.png" />
+              </div>
+              <div className="frame-29">
+                <p className="text-wrapper-16">Microsoft Certified: DevOps Engineer Expert</p>
+              </div>
+            </div>
+            <div className="frame-28">
+              <div className="image-wrapper">
+                <img className="image-2" alt="Image" src="/img/image-12.png" />
+              </div>
+              <div className="frame-29">
+                <p className="text-wrapper-16">Microsoft Certified: Azure Administrator Associate</p>
+              </div>
+            </div>
+            <div className="frame-28">
+              <div className="image-wrapper">
+                <img className="image-2" alt="Image" src="/img/image-13.png" />
+              </div>
+              <div className="frame-29">
+                <div className="text-wrapper-16">Microsoft Certified: Azure Fundamentals</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-30">
+          <div className="frame-13">
+            <div className="text-wrapper-2">Education</div>
+            <img className="line" alt="Line" src="/img/line-1.svg" />
+          </div>
+          <div className="frame-18">
+            <div className="frame-31">
+              <p className="text-wrapper-17">
+                University of Technology
+                <br />
+                2003 - 2018
+              </p>
+              <div className="frame-32">
+                <div className="text-wrapper-3">Computer Science</div>
+                <p className="text-wrapper-18">
+                  Designs and implements Azure infrastructure using Terraform. Automates the deployment and management
+                  of Kubernetes clusters. Proficient in developing cloud native solutions for efficient and reliable
+                  applications.
+                </p>
+              </div>
+            </div>
+            <img className="line" alt="Line" src="/img/line-5.svg" />
+            <div className="frame-33">
+              <div className="text-wrapper-17">
+                Tech Academy
+                <br />
+                2011 - 2012
+              </div>
+              <div className="frame-32">
+                <div className="text-wrapper-19">Solution Architecture and Design</div>
+                <p className="text-wrapper-18">
+                  Implements and manages Azure infrastructure using Terraform. Proficient in container orchestration
+                  with Kubernetes. Deploys cloud native solutions to optimize application performance.
+                </p>
+              </div>
+            </div>
+            <img className="line" alt="Line" src="/img/line-5.svg" />
+          </div>
+        </div>
+        <div className="frame-34">
+          <div className="frame-13">
+            <div className="text-wrapper-2">Get in touch</div>
+            <img className="line" alt="Line" src="/img/line-1.svg" />
+          </div>
+          <div className="frame-35">
+            <div className="frame-3">
+              <img className="vector-7" alt="Vector" src="/img/vector-2.svg" />
+              <div className="text-wrapper-20">loremipsum@gmail.com</div>
+            </div>
+            <div className="frame-3">
+              <img className="vector-8" alt="Vector" src="/img/vector-1.svg" />
+              <div className="text-wrapper-20">+40&nbsp;&nbsp;(768) 456 894</div>
+            </div>
+          </div>
+        </div>
+        <div className="bar">
+          <div className="frame-36">
+            <div className="MB-wrapper">
+              <img className="MB" alt="Mb" src="/img/mb-1.svg" />
+            </div>
+            <div className="text-wrapper-21">DevOps Solutions</div>
+          </div>
+          <img className="div-divim" alt="Div" src="/img/div-divim95-1.svg" />
+        </div>
+        <div className="bar-2">
+          <div className="frame-36">
+            <div className="MB-wrapper">
+              <img className="MB" alt="Mb" src="/img/mb.svg" />
+            </div>
+            <div className="text-wrapper-21">DevOps Solutions</div>
+          </div>
+          <img className="div-divim" alt="Div" src="/img/div-divim95.svg" />
+        </div>
+      </div>
+    </div>
+  );
+};
